73f41ea47207be97ad7d825e5eaf171ffbd823ba
Ahmad Zakaria <ahmadz1991@gmail.com>
2018-03-04 22:06:19 +0100

    override is_ai to True for abstract AI classes

--- ScotlandPYard/spyengine/aidetective.py
+++ ScotlandPYard/spyengine/aidetective.py
@@ -2,6 +2,9 @@ from .abstractdetective import AbstractDetective
 
 
 class AIDetective(AbstractDetective):
+    def __init__(self, engine, name=None):
+        super().__init__(engine, is_ai=True, name=name)
+
     def play_next(self):
         """TODO: insert AI logic here"""
         pass
--- ScotlandPYard/spyengine/aimrx.py
+++ ScotlandPYard/spyengine/aimrx.py
@@ -2,6 +2,9 @@ from .abstractmrx import AbstractMrX
 
 
 class AIDetective(AbstractMrX):
+    def __init__(self, engine, num_players=4):
+        super().__init__(engine, is_ai=True, num_players=num_players)
+
     def play_next(self):
         """TODO: insert AI logic here"""
         pass
