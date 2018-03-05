db79568776123af93c512d5c8d7fdb7c79bca781
Ahmad Zakaria Mohammad <ahmad.mohammad@fkie.fraunhofer.de>
2018-03-05 09:43:27 +0100

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='ScotlandPYard',
-    version='1.0.0',
+    version='1.0.1',
     description="A python implementation of the board game 'Scotland Yard'",
     long_description=readme + '\n\n' + history,
     author="Ahmad Zakaria M.",
